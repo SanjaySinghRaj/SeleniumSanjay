a05f27352b930f1fa82d7f2eaaf22b8d62ab94af
Bhagwan Singh <Bhagwan Singh@LAPTOP-UTR7A9HQ>
2021-07-27 13:12:02 +0530

    27july

--- src/july27/GoogleJava.java
+++ src/july27/GoogleJava.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class GoogleJava {
-	public static void main(String[] args)throws Throwable {
+	public static void main(String[] args) throws Throwable {
 		System.setProperty("webdriver.chrome.driver", "./Software/chromedriver.exe");
 		WebDriver demo = new ChromeDriver();
 		demo.manage().window().maximize();
@@ -18,18 +18,14 @@ public class GoogleJava {
 		demo.findElement(By.xpath("//input[@name='q']")).sendKeys("java");
 		Thread.sleep(1000);
 		List<WebElement> se = demo.findElements(By.xpath("//ul[@role='listbox']/li"));
-		Actions ac=new Actions(demo);
 		for (WebElement sea : se) {
 			System.out.println(sea.getText());
-			ac.moveToElement(sea).build().perform();
-			Thread.sleep(1000);
-			
+
 		}
 		Thread.sleep(2000);
-		
-	 demo.findElement(By.xpath("//span[text()='java']/b[.='script']")).click();
-	    
-	    demo.close();
-	
+		se.get(2).click();
+
+		demo.close();
+
 	}
 }
